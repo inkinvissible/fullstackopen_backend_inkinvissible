b1849263cb17bd21fbf05f0f0fda856013918ca9
inkinvissible <agusmartin.13.113@gmail.com>
2024-12-07 22:58:51 -0300

    Producción

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ morgan.token('body', (req) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 let phonebook = [
     {
@@ -39,10 +40,6 @@ let phonebook = [
 
 app.get('/api/persons', (request, response) => {
     response.json(phonebook)
-})
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/info', (request, response) => {
